8ff8d654d22e63c3e8ebb1bb071dcead469fcf52
SalmaAzmeena <150580792+SalmaAzmeena@users.noreply.github.com>
2024-08-06 15:31:47 +0530

    Initial commit

---
+++ config/header.php
@@ -0,0 +1,19 @@
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-dark navbar-light fixed-top">
+  <!-- Left navbar links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+     
+    </li>
+  </ul>
+  <a href="dashboard.php" class="navbar-brand">
+    <span class="brand-text font-weight-light">Clinical Data Management System </span>
+</a>
+  <!-- Right navbar links -->
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+    <div class="login-user text-light font-weight-bolder"> <?= $_SESSION['display_name'] ?>!</div>  
+    </li>
+  </ul>
+</nav>
+<!-- /.navbar -->
